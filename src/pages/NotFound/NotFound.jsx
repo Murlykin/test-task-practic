8f16f668e01d502abbdb9f4b88007e38e796cc7c
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-04-25 23:05:53 +0300

    finish

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,13 @@
-import { NotFounds } from './NotFound.styled';
+import GoIT from '../../images/GoIT_baner.jpg';
+import { GoItWrap, GoIt } from './NotFound.styled';
 
 export const NotFound = () => {
-  return <NotFounds>Щось пішло не так, спробуйте ще раз...</NotFounds>;
+  return (
+    <GoItWrap>
+      <GoIt src={GoIT} alt="picture" />
+    </GoItWrap>
+  );
 };
+
+//   // return <NotFounds>Щось пішло не так, спробуйте ще раз...</NotFounds>;
+// };
