70f97ea30ee19036639ad2fd0c144576e60b66d2
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-04-25 16:32:20 +0300

    Working

--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,10 +1,22 @@
-import styled from '@emotion/styled';
+import styled, {keyframes} from 'styled-components';
+
+const fadeIn = keyframes`
+from{
+opacity: 0;
+transform: translateX(-100%)
+}
+to{
+    opacity: 1;
+    transform: translateX(0);
+}
+`;
 
 export const GoItWrap = styled.div`
   display: flex;
   justify-content: center;
   padding: 8px 0;
   margin-top: 66px;
+   animation: ${fadeIn} 1.5s ease-out;
 `;
 
 export const GoIt = styled.img`
