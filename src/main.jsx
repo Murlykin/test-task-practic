75e2264962cc41dadab3f03a477494841dfee53b
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-04-26 08:26:19 +0300

    t

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 import { App } from './components/App/App.jsx';
 import './button-up';
 import './index.css';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <App />
+//    </HashRouter>
+//   </React.StrictMode>
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/test-task-practic">
       <App />
-   </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
